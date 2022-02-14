2eddc06b3f442172ee4a28a74de96b547d25362c
Emilien <emilien.pluvinage@gmail.com>
2022-02-14 11:11:14 +0100

    Ajout de newMove.php pour pouvoir cliquer une case

---
+++ table.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le :  lun. 14 fév. 2022 à 09:42
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données :  `tictactoe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `coup`
+--
+
+CREATE TABLE `coup` (
+  `id` int(11) NOT NULL,
+  `partie` int(11) NOT NULL,
+  `joueur` int(11) NOT NULL,
+  `abscisse` int(11) NOT NULL,
+  `ordonnee` int(11) NOT NULL,
+  `timestamp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `estDansLaPartie`
+--
+
+CREATE TABLE `estDansLaPartie` (
+  `id` int(11) NOT NULL,
+  `partie` int(11) NOT NULL,
+  `joueur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur`
+--
+
+CREATE TABLE `joueur` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `timestamp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `partie`
+--
+
+CREATE TABLE `partie` (
+  `id` int(11) NOT NULL,
+  `prochainCoup` int(11) NOT NULL,
+  `vainqueur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `coup`
+--
+ALTER TABLE `coup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `estDansLaPartie`
+--
+ALTER TABLE `estDansLaPartie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `partie`
+--
+ALTER TABLE `partie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `coup`
+--
+ALTER TABLE `coup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `estDansLaPartie`
+--
+ALTER TABLE `estDansLaPartie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `partie`
+--
+ALTER TABLE `partie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
