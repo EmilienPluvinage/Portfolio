29cc19bfc961abb11aa9ce7d5500c4f0753b805b
Emilien <emilien.pluvinage@gmail.com>
2022-03-07 14:39:01 +0100

    starting Till App with React. HTML and CSS so far

---
+++ till/src/components/TopMenu.js
@@ -0,0 +1,15 @@
+import "../styles/topmenu.css";
+
+function TopMenu() {
+  return (
+    <nav id="top-menu">
+      <a>Viennoiserie</a>
+      <a>Pâtisserie</a>
+      <a>Pain</a>
+      <a>Boissons</a>
+      <a>Snacking</a>
+    </nav>
+  );
+}
+
+export default TopMenu;
