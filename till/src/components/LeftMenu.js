8102dce29a148cb3cf400be0eb69c263b9448156
Emilien <emilien.pluvinage@gmail.com>
2022-03-09 18:29:52 +0100

    added tickets and employees list in left menu

--- till/src/components/LeftMenu.js
+++ till/src/components/LeftMenu.js
@@ -7,6 +7,20 @@ function LeftMenu() {
         <li>Configuration</li>
         <li>Statistics</li>
       </ul>
+      <div>RECEIPTS</div>
+      <div id="tickets-on-hold">
+        <div>Receipt 1</div>
+        <div>Receipt 2</div>
+        <div>Receipt 1</div>
+        <div>Receipt 2</div>
+        <div>Receipt 1</div>
+        <div>Receipt 2</div>
+      </div>
+      <div>EMPLOYEES</div>
+      <div id="tickets-on-hold">
+        <div>Employee 1</div>
+        <div>Employee 2</div>
+      </div>
     </nav>
   );
 }
