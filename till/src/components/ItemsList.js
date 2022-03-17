2ac5f7248193f932b60cbdb4c897b02967b422cf
Emilien <emilien.pluvinage@gmail.com>
2022-03-17 17:45:59 +0100

    direct sale button added

--- till/src/components/ItemsList.js
+++ till/src/components/ItemsList.js
@@ -1,15 +1,37 @@
 import "../styles/item.css";
 import { ItemData } from "../datas/ItemData";
 import { displayPrice } from "./Functions";
+import { useState } from "react";
+import InputDialog from "./InputDialog";
 
 function ItemsList(props) {
-  function updateState(price, name) {
+  const [inputDialog, updateInputDialog] = useState({
+    open: false,
+    name: "",
+    text: "",
+    callback: null,
+  });
+
+  function setExpanded(bool) {
+    updateInputDialog({
+      open: bool,
+      name: inputDialog.name,
+      text: inputDialog.text,
+      callback: inputDialog.callback,
+    });
+  }
+
+  function updateState(name, price, directSale) {
     var NewTicket = props.ticket;
     // we check if the article is already on the ticket, in which case we update the quantity, otherwise we add it
     var found = NewTicket.findIndex((e) => e.name === name);
     if (found !== -1) {
       // we found the article
-      NewTicket[found].quantity++;
+      if (directSale) {
+        NewTicket[found].price += parseInt(price);
+      } else {
+        NewTicket[found].quantity++;
+      }
     } else {
       NewTicket.push({
         name: name,
@@ -21,15 +43,31 @@ function ItemsList(props) {
     props.updateTicket(NewTicket);
     props.totalOfReceipt(NewTicket);
   }
+
+  function directSale(name, price) {
+    updateState(name, price, true);
+  }
+
+  function ClickOnDirectSale() {
+    updateInputDialog({
+      open: true,
+      name: "Vente Directe",
+      text: "Please enter a price in cents :",
+      callback: directSale,
+    });
+  }
   return (
     <div id="items">
+      <div style={{ textAlign: "center" }}>
+        <InputDialog options={inputDialog} setExpanded={setExpanded} />
+      </div>
       {ItemData.map(
         ({ name, category, price, id }) =>
           props.category === category && (
             <div
               key={id}
               className={"item " + props.darkmode}
-              onClick={() => updateState(Math.round(price), name)}
+              onClick={() => updateState(name, Math.round(price), false)}
             >
               <div className="item-content">
                 {name}
@@ -38,6 +76,13 @@ function ItemsList(props) {
             </div>
           )
       )}
+      <div
+        key="item-direct"
+        className={"item " + props.darkmode}
+        onClick={ClickOnDirectSale}
+      >
+        <div className="item-content">Vente Directe</div>
+      </div>
     </div>
   );
 }
