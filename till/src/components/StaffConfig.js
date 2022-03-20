17f25c986d3d1789b34ebddc519667d4cf64d6eb
Emilien <emilien.pluvinage@gmail.com>
2022-03-20 19:11:49 +0100

    deleteStaff() and Delete route

--- till/src/components/StaffConfig.js
+++ till/src/components/StaffConfig.js
@@ -20,19 +20,60 @@ function StaffConfig({ configMenu, updateConfigMenu, darkmode, EmployeeData }) {
     });
   }
 
+  function updateStaff(name) {
+    alert("Modifier " + name);
+  }
+
+  function deleteStaff(name) {
+    console.log("supprimer " + name);
+    fetch("http://localhost:3001/Staff-" + name, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  function addStaff() {
+    alert("Ajouter");
+  }
   return (
     configMenu === "Staff" && (
       <div id="items">
         <div style={{ textAlign: "center" }}>
           <InputDialog options={inputDialog} setExpanded={setExpanded} />
         </div>
+        <div className={"item " + darkmode} onClick={() => addStaff()}>
+          <div className="item-content">+ Add</div>
+        </div>
         {EmployeeData.map(({ _id, name, color }) => (
           <div key={_id} className={"config-item " + darkmode}>
             <div className="config-item-content">
               {name}
               <div className="config-buttons">
-                <div className={"config-btn " + darkmode}>Modify</div>
-                <div className={"config-btn " + darkmode}>Delete</div>
+                <div
+                  className={"config-btn " + darkmode}
+                  onClick={() => updateStaff(name)}
+                >
+                  Modify
+                </div>
+                <div
+                  className={"config-btn " + darkmode}
+                  onClick={() => deleteStaff(name)}
+                >
+                  Delete
+                </div>
               </div>
             </div>
           </div>
