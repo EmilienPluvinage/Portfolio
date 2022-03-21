c9a041141dda38bb48f6365bf7b480b35797c8b6
Emilien <emilien.pluvinage@gmail.com>
2022-03-21 16:30:29 +0100

    draft statistics page

---
+++ till/src/components/Statistics.js
@@ -0,0 +1,18 @@
+import "../styles/statistics.css";
+import { displayPrice } from "./Functions";
+import { useState } from "react";
+
+function Statistics(props) {
+  return (
+    <div id="statistics">
+      <div id="statistics-header"></div>
+      <div id="statistics-main">
+        <div className="statistics-third">a</div>
+        <div className="statistics-third">a</div>
+        <div className="statistics-third">a</div>
+      </div>
+    </div>
+  );
+}
+
+export default Statistics;
