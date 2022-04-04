3612126007e1c7dc44f30c47faf93d2c30142556
Emilien <emilien.pluvinage@gmail.com>
2022-04-04 18:00:21 +0200

    starting backend for cabinet-sante

---
+++ cabinet-sante-backend/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+//////////////
+//   READ   //
+//////////////
+
+// TEST SERVER
+
+app.get("/Test", (req, res, next) => {
+  console.log("Get request received");
+  res.end("Hello World\n");
+});
+//
+
+module.exports = app;
