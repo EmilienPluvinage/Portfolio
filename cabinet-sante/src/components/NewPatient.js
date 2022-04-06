9b096d9c2ed48d2810031707d0b3bf38189b5ac8
Emilien <emilien.pluvinage@gmail.com>
2022-04-06 19:42:30 +0200

    started form

--- cabinet-sante/src/components/NewPatient.js
+++ cabinet-sante/src/components/NewPatient.js
@@ -3,16 +3,53 @@ import "../styles/styles.css";
 export default function NewPatient() {
   return (
     <div>
-      <h2>État Civil du Patient</h2>
-      <div className="main-content">1</div>
-      <h2>Informations sur le Patient</h2>
-      <div className="main-content">2</div>
-      <h2>Antécédents</h2>
-      <div className="main-content">3</div>
-      <h2>Documents</h2>
-      <div className="main-content">4</div>
-      <h2>Ajouter une consultation aujourd'hui</h2>
-      <div className="main-content">5</div>
+      <form>
+        <h2>1 - État Civil du Patient</h2>
+        <div className="main-content">
+          <p>
+            Nom: <input type="text" name="lastname" />
+          </p>
+          <p>
+            Prénom: <input type="text" name="firstname" />
+          </p>
+          <p>
+            Date de naissance: <input type="text" name="birthday" />
+          </p>
+          <p>
+            Genre: <input type="text" name="sexe" />
+          </p>
+          <p>
+            Téléphone portable: <input type="text" name="mobilephone" />
+          </p>
+          <p>
+            Téléphone fixe: <input type="text" name="landline" />
+          </p>
+          <p>
+            Adresse e-mail: <input type="text" name="email" />
+          </p>
+          <p>
+            Adresse postale: <textarea name="address"> </textarea>
+          </p>
+          <p>
+            Code postal: <input type="text" name="postcode" />
+          </p>
+          <p>
+            Ville: <input type="text" name="city" />
+          </p>
+
+          <p>
+            Remarques:<textarea name="comments"> </textarea>
+          </p>
+        </div>
+        <h2>2 - Informations sur le Patient</h2>
+        <div className="main-content">2</div>
+        <h2>3 - Antécédents</h2>
+        <div className="main-content">3</div>
+        <h2>4 - Documents</h2>
+        <div className="main-content">4</div>
+        <h2>5 - Ajouter une consultation aujourd'hui</h2>
+        <div className="main-content">5</div>
+      </form>
     </div>
   );
 }
