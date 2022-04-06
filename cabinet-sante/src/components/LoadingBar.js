614640fcf43d6ee952e35e7b971efd47fdab619a
Emilien <emilien.pluvinage@gmail.com>
2022-04-06 20:33:54 +0200

    added loading bar

---
+++ cabinet-sante/src/components/LoadingBar.js
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-right: 10px;
+  margin-left: 10px;
+  background-color: grey;
+  display: flex;
+  flex-direction: row;
+  height: 5px;
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const Color = styled.div`
+  width: ${(props) => props.size}%;
+  background: ${(props) => props.color};
+  transition: width 500ms;
+`;
+const Gray = styled.div`
+  background-color: grey;
+  flex: 1;
+`;
+
+function LoadingBar({ percentage, color }) {
+  return (
+    <Container>
+      <Color color={color} size={percentage} />
+      <Gray />
+    </Container>
+  );
+}
+
+LoadingBar.propTypes = {
+  percentage: PropTypes.number.isRequired,
+  color: PropTypes.string,
+};
+
+LoadingBar.defaultProps = {
+  color: "GreenYellow",
+};
+
+export default LoadingBar;
