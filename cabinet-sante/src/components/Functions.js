469b52d2bb99c608e2125432dea8bf709cf0cc47
Emilien <emilien.pluvinage@gmail.com>
2022-04-07 16:29:26 +0200

    imported personnal utility functions

---
+++ cabinet-sante/src/components/Functions.js
@@ -0,0 +1,60 @@
+export function displayPrice(priceInCents) {
+  // displays the price with a comma and two decimals, unless it's 0, in which case we return 0.
+  var i = Math.round(priceInCents).toString().length - 2;
+  var result =
+    priceInCents === 0
+      ? 0
+      : (i > 0 ? priceInCents.toString().slice(0, i) : "0") +
+        "," +
+        (i === -1 ? "0" : "") +
+        priceInCents.toString().slice(i, i + 2);
+  return result;
+}
+
+export function displayPercentage(value) {
+  return (Math.round(value * 10000) / 100).toString() + "%";
+}
+
+export function displayDate(today, dateOnly) {
+  var month = today.getMonth() + 1;
+
+  var date =
+    today.getDate() +
+    "-" +
+    (month.toString().length === 1 ? "0" + month : month) +
+    "-" +
+    today.getFullYear() +
+    (!dateOnly
+      ? " " +
+        today.getHours() +
+        ":" +
+        (today.getMinutes().toString().length === 1
+          ? "0" + today.getMinutes()
+          : today.getMinutes())
+      : "");
+
+  return date;
+}
+
+export function displayTime(today) {
+  var date =
+    today.getHours() +
+    ":" +
+    (today.getMinutes().toString().length === 1
+      ? "0" + today.getMinutes()
+      : today.getMinutes());
+
+  return date;
+}
+
+export const datesAreOnSameDay = (first, second) =>
+  first.getFullYear() === second.getFullYear() &&
+  first.getMonth() === second.getMonth() &&
+  first.getDate() === second.getDate();
+
+export function capitalize(string) {
+  if (string?.length > 0) {
+    const lower = string.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  }
+}
