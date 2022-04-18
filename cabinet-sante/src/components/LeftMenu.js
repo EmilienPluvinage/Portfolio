a99edba521c3e9f3516f5d1efacac738b01a69b4
Emilien <emilien.pluvinage@gmail.com>
2022-04-18 09:15:22 +0200

    new appointment can be opened from left menu

--- cabinet-sante/src/components/LeftMenu.js
+++ cabinet-sante/src/components/LeftMenu.js
@@ -12,9 +12,11 @@ import {
   CalendarStats,
   Settings,
   Search,
+  ReportMedical,
 } from "tabler-icons-react";
-import { Center, Select } from "@mantine/core";
+import { Center, Select, Modal } from "@mantine/core";
 import { useState } from "react";
+import NewAppointment from "./NewAppointment";
 
 function LeftMenu() {
   const navigate = useNavigate();
@@ -22,6 +24,7 @@ function LeftMenu() {
   const patientsList = patients.map((e) => {
     return e.fullname;
   });
+  const [opened, setOpened] = useState(false);
   const [search, setSearch] = useState("");
   const path = useLocation().pathname;
   const loggedIn = useLogin().login;
@@ -43,73 +46,97 @@ function LeftMenu() {
   }
   return (
     loggedIn && (
-      <div id="LeftMenu">
-        <Center>
-          <Select
-            limit={5}
-            searchable
-            placeholder="Rechercher un patient"
-            data={patientsList}
-            icon={<Search size={18} />}
-            style={{
-              marginTop: "20px",
-              marginLeft: "20px",
-              marginBottom: "10px",
-            }}
-            value={search}
-            onChange={handleSearch}
-          />
-        </Center>
-        <ul>
-          <Link to="/" className="text-link">
-            <li className={path === "/" ? "clicked" : ""}>
-              <Home size={iconsSize} style={iconsStyle} />
-              Accueil
-            </li>
-          </Link>
-          <Link to="/Nouveau-Patient" className="text-link">
-            <li className={path === "/Nouveau-Patient" ? "clicked" : ""}>
-              <UserPlus size={iconsSize} style={iconsStyle} />
-              Nouveau Patient
-            </li>
-          </Link>
-          <Link to="/Listing-Patients" className="text-link">
-            <li className={path === "/Listing-Patients" ? "clicked" : ""}>
-              <List size={iconsSize} style={iconsStyle} />
-              Liste des patients
-            </li>
-          </Link>
-          <Link to="/Agenda" className="text-link">
-            <li className={path === "/Agenda" ? "clicked" : ""}>
-              <CalendarStats size={iconsSize} style={iconsStyle} />
-              Agenda
-            </li>
-          </Link>
-          <Link to="/Comptabilite" className="text-link">
-            <li className={path === "/Comptabilite" ? "clicked" : ""}>
-              <Calculator size={iconsSize} style={iconsStyle} />
-              Comptabilité
-            </li>
-          </Link>
-          <Link to="/Statistiques" className="text-link">
-            <li className={path === "/Statistiques" ? "clicked" : ""}>
-              <ChartBar size={iconsSize} style={iconsStyle} />
-              Statistiques
-            </li>
-          </Link>
-          <Link to="/Parametres" className="text-link">
-            <li className={path === "/Parametres" ? "clicked" : ""}>
-              <Settings size={iconsSize} style={iconsStyle} />
-              Paramètres
-            </li>
-          </Link>
+      <>
+        <Modal
+          centered
+          overlayOpacity={0.3}
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title={"Consultation"}
+          closeOnClickOutside={false}
+        >
+          {opened && (
+            <NewAppointment
+              setOpened={setOpened}
+              startingTime={0}
+              patientId={0}
+              appointmentId={0}
+            />
+          )}
+        </Modal>
 
-          <li onClick={() => logging(false)}>
-            <Logout size={iconsSize} style={iconsStyle} />
-            Déconnexion
-          </li>
-        </ul>
-      </div>
+        <div id="LeftMenu">
+          <Center>
+            <Select
+              limit={5}
+              searchable
+              placeholder="Rechercher un patient"
+              data={patientsList}
+              icon={<Search size={18} />}
+              style={{
+                marginTop: "20px",
+                marginLeft: "20px",
+                marginBottom: "10px",
+              }}
+              value={search}
+              onChange={handleSearch}
+            />
+          </Center>
+          <ul>
+            <Link to="/" className="text-link">
+              <li className={path === "/" ? "clicked" : ""}>
+                <Home size={iconsSize} style={iconsStyle} />
+                Accueil
+              </li>
+            </Link>
+            <Link to="/Nouveau-Patient" className="text-link">
+              <li className={path === "/Nouveau-Patient" ? "clicked" : ""}>
+                <UserPlus size={iconsSize} style={iconsStyle} />
+                Nouveau Patient
+              </li>
+            </Link>
+            <li onClick={() => setOpened(true)}>
+              <ReportMedical size={iconsSize} style={iconsStyle} />
+              Nouvelle Consultation
+            </li>
+            <Link to="/Listing-Patients" className="text-link">
+              <li className={path === "/Listing-Patients" ? "clicked" : ""}>
+                <List size={iconsSize} style={iconsStyle} />
+                Liste des patients
+              </li>
+            </Link>
+            <Link to="/Agenda" className="text-link">
+              <li className={path === "/Agenda" ? "clicked" : ""}>
+                <CalendarStats size={iconsSize} style={iconsStyle} />
+                Agenda
+              </li>
+            </Link>
+            <Link to="/Comptabilite" className="text-link">
+              <li className={path === "/Comptabilite" ? "clicked" : ""}>
+                <Calculator size={iconsSize} style={iconsStyle} />
+                Comptabilité
+              </li>
+            </Link>
+            <Link to="/Statistiques" className="text-link">
+              <li className={path === "/Statistiques" ? "clicked" : ""}>
+                <ChartBar size={iconsSize} style={iconsStyle} />
+                Statistiques
+              </li>
+            </Link>
+            <Link to="/Parametres" className="text-link">
+              <li className={path === "/Parametres" ? "clicked" : ""}>
+                <Settings size={iconsSize} style={iconsStyle} />
+                Paramètres
+              </li>
+            </Link>
+
+            <li onClick={() => logging(false)}>
+              <Logout size={iconsSize} style={iconsStyle} />
+              Déconnexion
+            </li>
+          </ul>
+        </div>
+      </>
     )
   );
 }
