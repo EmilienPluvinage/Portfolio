2f7efa96c2cdaeccfbce47577bc80492c21e5a0f
Emilien <emilien.pluvinage@gmail.com>
2022-04-21 13:12:02 +0200

    Confirmation Pop Up component

---
+++ cabinet-sante/src/components/Confirmation.js
@@ -0,0 +1,37 @@
+import { Button, Modal, Grid, Text } from "@mantine/core";
+
+export default function Confirmation({ text, title, callback, open, close }) {
+  function handleClick() {
+    callback();
+    close();
+  }
+
+  return (
+    open && (
+      <Modal
+        centered
+        overlayOpacity={0.3}
+        opened={open}
+        onClose={close}
+        title={title}
+        closeOnClickOutside={false}
+        withCloseButton={false}
+      >
+        <Text>{text}</Text>
+        <Grid
+          justify="space-between"
+          style={{ marginTop: "10px", marginRight: "45px" }}
+        >
+          <Grid.Col span={2}>
+            <Button variant="default" onClick={close}>
+              Retour
+            </Button>
+          </Grid.Col>
+          <Grid.Col span={2}>
+            <Button onClick={handleClick}>Continuer</Button>
+          </Grid.Col>
+        </Grid>
+      </Modal>
+    )
+  );
+}
