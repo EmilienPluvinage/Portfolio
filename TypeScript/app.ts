bbdccd821b894743fd85d16734dd94feae8dd7fe
Emilien <emilien.pluvinage@gmail.com>
2022-05-03 18:15:15 +0200

    using types

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,1 +1,11 @@
-console.log("test");
+function add(n1: number, n2: number, showNumber: boolean) {
+  if (showNumber) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
+}
+
+const number1 = "5";
+const number2 = 2.8;
+add(+number1, +number2, true);
