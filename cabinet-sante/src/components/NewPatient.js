b386d57e166abd4f940f99e2874c891f508f1f99
Emilien <emilien.pluvinage@gmail.com>
2022-05-09 17:25:02 +0200

    forms for pathologies done

--- cabinet-sante/src/components/NewPatient.js
+++ cabinet-sante/src/components/NewPatient.js
@@ -52,6 +52,8 @@ export default function NewPatient() {
   const getPatients = useUpdatePatients().update;
   const [id, setId] = useState(0);
   const params = useParams();
+  const pathologies = useConfig().pathologies;
+  console.log(pathologies);
   const initialValues = {
     lastname: "",
     firstname: "",
@@ -492,6 +494,199 @@ export default function NewPatient() {
                 <Checkbox value="gauche" label="Gauche" />
                 <Checkbox value="droite" label="Droite" />
               </CheckboxGroup>
+            </div>
+          </div>
+        </div>
+        <h2>Antécédents</h2>
+        <div className="main-content">
+          <div className="new-patient">
+            <div className="form-column">
+              <table style={{ width: "100%" }}>
+                <thead>
+                  <tr>
+                    <th
+                      colSpan={3}
+                      style={{
+                        border: "1px solid lightgray",
+                        background: "lightgray",
+                        padding: "5px",
+                        borderRadius: "5px",
+                      }}
+                    >
+                      {pathologies.find((e) => e.groupId === 1)?.name}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pathologies
+                    .filter((e) => e.groupId === 1)
+                    .map((element) => (
+                      <tr key={element.pathology}>
+                        <td style={{ textAlign: "right" }}>
+                          {element.pathology}
+                        </td>
+                        <td>
+                          <Checkbox />
+                        </td>
+                        <td>
+                          <Textarea
+                            rows={1}
+                            maxRows={1}
+                            minRows={1}
+                            style={{ lineHeight: "normal" }}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+              <table>
+                <thead>
+                  <tr>
+                    <th
+                      colSpan={3}
+                      style={{
+                        border: "1px solid lightgray",
+                        padding: "5px",
+                        background: "lightgray",
+                        borderRadius: "5px",
+                      }}
+                    >
+                      {pathologies.find((e) => e.groupId === 5)?.name}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pathologies
+                    .filter((e) => e.groupId === 5)
+                    .map((element) => (
+                      <tr key={element.pathology}>
+                        <td style={{ textAlign: "right" }}>
+                          {element.pathology}
+                        </td>
+                        <td>
+                          <Checkbox />
+                        </td>
+                        <td>
+                          <Textarea
+                            rows={1}
+                            maxRows={1}
+                            minRows={1}
+                            style={{ lineHeight: "normal" }}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="form-column">
+              <table>
+                <thead>
+                  <tr>
+                    <th
+                      colSpan={3}
+                      style={{
+                        border: "1px solid lightgray",
+                        padding: "5px",
+                        background: "lightgray",
+                        borderRadius: "5px",
+                      }}
+                    >
+                      {pathologies.find((e) => e.groupId === 6)?.name}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pathologies
+                    .filter((e) => e.groupId === 6)
+                    .map((element) => (
+                      <tr key={element.pathology}>
+                        <td style={{ textAlign: "right" }}>
+                          {element.pathology}
+                        </td>
+                        <td>
+                          <Checkbox />
+                        </td>
+                        <td>
+                          <Textarea
+                            rows={1}
+                            maxRows={1}
+                            minRows={1}
+                            style={{ lineHeight: "normal" }}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="form-column">
+              <table>
+                <thead>
+                  <tr>
+                    <th
+                      colSpan={3}
+                      style={{
+                        border: "1px solid lightgray",
+                        padding: "5px",
+                        background: "lightgray",
+                        borderRadius: "5px",
+                      }}
+                    >
+                      {pathologies.find((e) => e.groupId === 7)?.name}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pathologies
+                    .filter((e) => e.groupId === 7)
+                    .map((element) => (
+                      <tr key={element.pathology}>
+                        <td style={{ textAlign: "right" }}>
+                          {element.pathology}
+                        </td>
+                        <td>
+                          <Checkbox />
+                        </td>
+                        <td>
+                          <Textarea
+                            rows={1}
+                            maxRows={1}
+                            minRows={1}
+                            style={{ lineHeight: "normal" }}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="new-patient">
+            <div className="form-column">
+              {pathologies
+                .filter((e) => e.groupId === 0)
+                .slice(0, 3)
+                .map((element) => (
+                  <Textarea label={element.pathology} />
+                ))}
+            </div>
+            <div className="form-column">
+              {pathologies
+                .filter((e) => e.groupId === 0)
+                .slice(3, 6)
+                .map((element) => (
+                  <Textarea label={element.pathology} />
+                ))}
+            </div>
+            <div className="form-column">
+              {pathologies
+                .filter((e) => e.groupId === 0)
+                .slice(6)
+                .map((element) => (
+                  <Textarea label={element.pathology} />
+                ))}
             </div>
           </div>
         </div>
