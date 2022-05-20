8d40a7e8686d377739c9d6a4eea258c3cbe9b62a
Emilien <emilien.pluvinage@gmail.com>
2022-05-20 19:22:55 +0200

    remove relative backend

--- cabinet-sante-backend/routes/patients.js
+++ cabinet-sante-backend/routes/patients.js
@@ -516,4 +516,33 @@ router.post("/DelinkPatients", (req, res, next) => {
   });
 });
 
+router.post("/RemoveRelative", (req, res, next) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.query(
+      "SELECT userId FROM tokens WHERE token= ?",
+      req.body.token,
+      (err, rows) => {
+        connection.release(); // return the connection to pool
+        if (err) throw err;
+        if (rows.length === 1) {
+          var userId = rows[0].userId;
+          // belongs to that user
+          // Now connected and we have the user ID so we do the insert
+          connection.query(
+            "DELETE FROM IsRelativeWith WHERE id=? AND userId=?",
+            [req.body.id, userId],
+            (err, result) => {
+              res.status(201).json({ success: true, error: "" });
+            }
+          );
+        } else {
+          res.status(201).json({ success: false, error: "not connected" });
+        }
+      }
+    );
+  });
+});
+
 module.exports = router;
