5926c72d364f785f9fb851ba43387de7dae0a0d3
Emilien <emilien.pluvinage@gmail.com>
2022-05-30 15:02:05 +0200

    styling

--- cabinet-sante/src/components/ReminderWarnings.js
+++ cabinet-sante/src/components/ReminderWarnings.js
@@ -1,6 +1,7 @@
 import "../styles/styles.css";
 import { usePatients } from "./contexts/PatientsContext";
 import { Text } from "@mantine/core";
+import { AlertCircle } from "tabler-icons-react";
 
 export default function ReminderWarnings() {
   // data from context
@@ -13,8 +14,13 @@ export default function ReminderWarnings() {
       <div className="home-content">
         {reminders.map((element) => (
           <Text size="sm">
+            <AlertCircle
+              size={18}
+              color="red"
+              style={{ position: "relative", top: "4px", right: "5px" }}
+            />
             {patients.find((e) => e.id === element.patientId)?.fullname}:{" "}
-            {element.description}
+            <span style={{ fontWeight: 500 }}>{element.description}</span>
           </Text>
         ))}
       </div>
