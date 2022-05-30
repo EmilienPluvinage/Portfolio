5201819602f66533f5ea9b1b9ad3d783edeaad2b
Emilien <emilien.pluvinage@gmail.com>
2022-05-30 19:19:22 +0200

    currently no reminder msg

--- cabinet-sante/src/components/ReminderWarnings.js
+++ cabinet-sante/src/components/ReminderWarnings.js
@@ -12,17 +12,21 @@ export default function ReminderWarnings() {
     <div>
       <h2>Rappels</h2>
       <div className="home-content">
-        {reminders.map((element) => (
-          <Text size="sm">
-            <AlertCircle
-              size={18}
-              color="red"
-              style={{ position: "relative", top: "4px", right: "5px" }}
-            />
-            {patients.find((e) => e.id === element.patientId)?.fullname}:{" "}
-            <span style={{ fontWeight: 500 }}>{element.description}</span>
-          </Text>
-        ))}
+        {reminders.length === 0 ? (
+          <Text size="sm">Aucun rappel actuellement</Text>
+        ) : (
+          reminders.map((element) => (
+            <Text size="sm">
+              <AlertCircle
+                size={18}
+                color="red"
+                style={{ position: "relative", top: "4px", right: "5px" }}
+              />
+              {patients.find((e) => e.id === element.patientId)?.fullname}:{" "}
+              <span style={{ fontWeight: 500 }}>{element.description}</span>
+            </Text>
+          ))
+        )}
       </div>
     </div>
   );
