f4a23dc2619909e72a8b034c3d2310bffc646a48
Emilien <emilien.pluvinage@gmail.com>
2022-06-30 11:34:37 +0200

    added Drawer

--- cabinet-sante-mobile/App.js
+++ cabinet-sante-mobile/App.js
@@ -3,15 +3,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import NewPatient from "./NewPatient";
 import Main from "./Main";
-import Header from "./Header";
-
+import { StatusBar } from "react-native";
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="light" />
       <PaperProvider>
-        <Drawer.Navigator initialRouteName="Main">
+        <Drawer.Navigator
+          initialRouteName="Main"
+          screenOptions={{
+            drawerInactiveBackgroundColor: "rgb(60,60,60)",
+            drawerInactiveTintColor: "#ffffff",
+            drawerActiveTintColor: "#ffffff",
+            drawerActiveBackgroundColor: "#22b8cf",
+            drawerStyle: { backgroundColor: "rgb(30,30,30)" },
+          }}
+        >
           <Drawer.Screen name="NewPatient" component={NewPatient} />
           <Drawer.Screen name="Main" component={Main} />
         </Drawer.Navigator>
