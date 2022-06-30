5c7c82859edf661356d7b448c31feaea7e4cc401
Emilien <emilien.pluvinage@gmail.com>
2022-06-30 12:15:46 +0200

    added icons

--- cabinet-sante-mobile/App.js
+++ cabinet-sante-mobile/App.js
@@ -4,6 +4,8 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import NewPatient from "./NewPatient";
 import Main from "./Main";
 import { StatusBar, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
 const Drawer = createDrawerNavigator();
 
 export default function App() {
@@ -17,13 +19,38 @@ export default function App() {
             drawerInactiveBackgroundColor: "rgb(60,60,60)",
             drawerInactiveTintColor: "#ffffff",
             drawerActiveTintColor: "#ffffff",
-            drawerActiveBackgroundColor: "#22b8cf",
+            drawerActiveBackgroundColor: "#1098AD",
             drawerStyle: { backgroundColor: "rgb(30,30,30)" },
             sceneContainerStyle: styles.container,
+            headerTintColor: "#1098AD",
+            headerStyle: {
+              backgroundColor: "rgb(30,30,30)",
+              shadowColor: "transparent",
+            },
           }}
         >
-          <Drawer.Screen name="NewPatient" component={NewPatient} />
-          <Drawer.Screen name="Main" component={Main} />
+          <Drawer.Screen
+            name="Main"
+            component={Main}
+            options={{
+              drawerLabel: "Agenda",
+              headerTitle: "Mon Cabinet Santé",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="home-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="NewPatient"
+            component={NewPatient}
+            options={{
+              drawerLabel: "Nouveau Patient",
+              headerTitle: "Nouveau Patient",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="person-add-outline" color={color} size={size} />
+              ),
+            }}
+          />
         </Drawer.Navigator>
       </PaperProvider>
     </NavigationContainer>
