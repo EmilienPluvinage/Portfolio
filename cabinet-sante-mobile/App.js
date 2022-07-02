f44691f5e774a9b57e802354f5afef6e77413cd7
Emilien <emilien.pluvinage@gmail.com>
2022-07-02 10:03:29 +0200

    added context providers

--- cabinet-sante-mobile/App.js
+++ cabinet-sante-mobile/App.js
@@ -1,44 +1,12 @@
 import { Provider as PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
-} from "@react-navigation/drawer";
-import NewPatient from "./NewPatient";
-import NewAppointment from "./NewAppointment";
-import Main from "./Main";
-import { StatusBar, StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import Login from "./Login";
-import { useState } from "react";
-import { REACT_APP_API_DOMAIN } from "@env";
+import { StatusBar } from "react-native";
 import { AuthProvider } from "./contexts/AuthContext";
 import { PatientsProvider } from "./contexts/PatientsContext";
 import { ConfigurationProvider } from "./contexts/ConfigContext";
-
-function CustomDrawerContent(props) {
-  return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      <DrawerItem
-        inactiveTintColor="#ffffff"
-        label="Déconnexion"
-        onPress={() => props.setLogin(false)}
-        icon={({ color, size }) => (
-          <Ionicons name="log-out-outline" color={color} size={size} />
-        )}
-      />
-    </DrawerContentScrollView>
-  );
-}
-
-const Drawer = createDrawerNavigator();
+import Drawer from "./Drawer";
 
 export default function App() {
-  const [login, setLogin] = useState(false);
-
   return (
     <NavigationContainer>
       <StatusBar style="light" />
@@ -46,75 +14,7 @@ export default function App() {
         <ConfigurationProvider>
           <PatientsProvider>
             <AuthProvider>
-              {login ? (
-                <Drawer.Navigator
-                  drawerContent={(props) => (
-                    <CustomDrawerContent {...props} setLogin={setLogin} />
-                  )}
-                  initialRouteName="Main"
-                  screenOptions={{
-                    drawerInactiveBackgroundColor: "rgb(30,30,30)",
-                    drawerInactiveTintColor: "#ffffff",
-                    drawerActiveTintColor: "#ffffff",
-                    drawerActiveBackgroundColor: "#1098AD",
-                    drawerStyle: { backgroundColor: "rgb(30,30,30)" },
-                    sceneContainerStyle: styles.container,
-                    headerTintColor: "#1098AD",
-                    headerStyle: {
-                      backgroundColor: "rgb(30,30,30)",
-                      shadowColor: "transparent",
-                    },
-                  }}
-                >
-                  <Drawer.Screen
-                    name="Main"
-                    component={Main}
-                    options={{
-                      drawerLabel: "Agenda",
-                      headerTitle: "Mon Cabinet Santé",
-                      drawerIcon: ({ color, size }) => (
-                        <Ionicons
-                          name="home-outline"
-                          color={color}
-                          size={size}
-                        />
-                      ),
-                    }}
-                  />
-                  <Drawer.Screen
-                    name="NewPatient"
-                    component={NewPatient}
-                    options={{
-                      drawerLabel: "Nouveau Patient",
-                      headerTitle: "Patient",
-                      drawerIcon: ({ color, size }) => (
-                        <Ionicons
-                          name="person-add-outline"
-                          color={color}
-                          size={size}
-                        />
-                      ),
-                    }}
-                  />
-                  <Drawer.Screen
-                    name="NewAppointment"
-                    component={NewAppointment}
-                    options={{
-                      drawerLabel: "Consultation",
-                      headerTitle: "Consultation",
-                      drawerIcon: ({ color, size }) => (
-                        <Ionicons
-                          name="calendar-outline"
-                          color={color}
-                          size={size}
-                        />
-                      ),
-                    }}
-                  />
-                </Drawer.Navigator>
-              ) : (
-                <Login login={login} setLogin={setLogin} />
-              )}
+              <Drawer />
             </AuthProvider>
           </PatientsProvider>
         </ConfigurationProvider>
@@ -122,13 +22,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "rgb(30,30,30)",
-    alignItems: "stretch",
-    justifyContent: "flex-start",
-    padding: 5,
-  },
-});
---
+++ cabinet-sante-mobile/Drawer.js
@@ -0,0 +1,108 @@
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import NewPatient from "./NewPatient";
+import NewAppointment from "./NewAppointment";
+import Main from "./Main";
+import { StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Login from "./Login";
+
+import { useLogin } from "./contexts/AuthContext";
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        inactiveTintColor="#ffffff"
+        label="Déconnexion"
+        onPress={() => props.setLogin(false)}
+        icon={({ color, size }) => (
+          <Ionicons name="log-out-outline" color={color} size={size} />
+        )}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  const loggedIn = useLogin().login;
+
+  return (
+    <>
+      {loggedIn ? (
+        <Drawer.Navigator
+          drawerContent={(props) => (
+            <CustomDrawerContent {...props} setLogin={setLogin} />
+          )}
+          initialRouteName="Main"
+          screenOptions={{
+            drawerInactiveBackgroundColor: "rgb(30,30,30)",
+            drawerInactiveTintColor: "#ffffff",
+            drawerActiveTintColor: "#ffffff",
+            drawerActiveBackgroundColor: "#1098AD",
+            drawerStyle: { backgroundColor: "rgb(30,30,30)" },
+            sceneContainerStyle: styles.container,
+            headerTintColor: "#1098AD",
+            headerStyle: {
+              backgroundColor: "rgb(30,30,30)",
+              shadowColor: "transparent",
+            },
+          }}
+        >
+          <Drawer.Screen
+            name="Main"
+            component={Main}
+            options={{
+              drawerLabel: "Agenda",
+              headerTitle: "Mon Cabinet Santé",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="home-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="NewPatient"
+            component={NewPatient}
+            options={{
+              drawerLabel: "Nouveau Patient",
+              headerTitle: "Patient",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="person-add-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="NewAppointment"
+            component={NewAppointment}
+            options={{
+              drawerLabel: "Consultation",
+              headerTitle: "Consultation",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="calendar-outline" color={color} size={size} />
+              ),
+            }}
+          />
+        </Drawer.Navigator>
+      ) : (
+        <Login />
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(30,30,30)",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+    padding: 5,
+  },
+});
