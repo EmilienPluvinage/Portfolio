527c172460b1e98593608aee54b5284c58eaaa36
Emilien <emilien.pluvinage@gmail.com>
2022-07-03 15:29:08 +0200

    display number of patients to renew in drawer item

--- cabinet-sante-mobile/Drawer.js
+++ cabinet-sante-mobile/Drawer.js
@@ -8,12 +8,18 @@ import NewPatient from "./NewPatient";
 import NewAppointment from "./NewAppointment";
 import Main from "./Main";
 import Reminders from "./Reminders";
+import Balance from "./Balance";
 import { StyleSheet, Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import Login from "./Login";
 import { Badge } from "react-native-paper";
 import { useLogin, useLogging } from "./contexts/AuthContext";
 import { usePatients } from "./contexts/PatientsContext";
+import {
+  insertPackageIntoArray,
+  calculateBalance,
+  getUniqueSharedPatients,
+} from "./Functions/Functions";
 
 function CustomDrawerContent(props) {
   return (
@@ -38,7 +44,56 @@ export default function App() {
   const loggedIn = useLogin().login;
   const logging = useLogging();
   const reminders = usePatients().reminders;
+  const patients = usePatients().patients;
+  const payements = usePatients().payements;
+  const appointments = usePatients().appointments;
+  const sharedBalance = usePatients().sharedBalance;
 
+  // used to exclude future appointments into balance calculation
+  const today = new Date();
+
+  function BalanceByPatient(patientId) {
+    const sharedPatients = getUniqueSharedPatients(sharedBalance, patientId);
+    const FilteredAppointments = appointments.filter(
+      (e) => sharedPatients.findIndex((f) => f === e.patientId) !== -1
+    );
+    const FilteredPayements = payements.filter(
+      (e) => sharedPatients.findIndex((f) => f === e.patientId) !== -1
+    );
+    const packagesData = FilteredPayements.filter((e) => e.eventId === 0).map(
+      (obj) => ({
+        ...obj,
+        dataType: "package",
+      })
+    );
+
+    var data = FilteredAppointments.map((obj) => ({
+      ...obj,
+      dataType: "event",
+    }));
+
+    packagesData.forEach((e) => insertPackageIntoArray(data, e));
+
+    calculateBalance(data, payements);
+
+    const balanceAsOfToday = data.filter(
+      (element) =>
+        new Date(element?.date) <= today || new Date(element?.start) <= today
+    )[0]?.balance;
+
+    if (balanceAsOfToday) {
+      return balanceAsOfToday;
+    } else {
+      return 0;
+    }
+  }
+
+  let count = 0;
+  for (const patient of patients) {
+    if (BalanceByPatient(patient?.id) < 0) {
+      count++;
+    }
+  }
   return (
     <>
       {loggedIn ? (
@@ -130,6 +185,33 @@ export default function App() {
                   color={color}
                   size={size}
                 />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="Balance"
+            component={Balance}
+            options={{
+              drawerLabel: ({ focused }) => (
+                <View
+                  style={{
+                    backgroundColor: "transparent",
+                    flexDirection: "row",
+                  }}
+                >
+                  <Text style={styles.drawerLabelText}>Renouveler</Text>
+                  <Badge
+                    style={{
+                      backgroundColor: !focused ? "#1098AD" : "rgb(30,30,30)",
+                    }}
+                  >
+                    {count}
+                  </Badge>
+                </View>
+              ),
+              headerTitle: "Solde",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="card-outline" color={color} size={size} />
               ),
             }}
           />
