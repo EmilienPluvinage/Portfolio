d682e71016d49ae1600982e567e9c8b7ee4d1441
Emilien <emilien.pluvinage@gmail.com>
2022-07-04 14:16:53 +0200

    added checks and snackbar

--- cabinet-sante-mobile/NewPatient.js
+++ cabinet-sante-mobile/NewPatient.js
@@ -6,12 +6,15 @@ import {
   RadioButton,
   Text,
   Button,
+  Snackbar,
 } from "react-native-paper";
 import { REACT_APP_API_DOMAIN } from "@env";
 import { useLogin } from "./contexts/AuthContext";
-import { useUpdatePatients } from "./contexts/PatientsContext";
+import { usePatients, useUpdatePatients } from "./contexts/PatientsContext";
+import { capitalize } from "./Functions/Functions";
 
 export default function NewPatient() {
+  const patients = usePatients().patients;
   const [name, setName] = useState("");
   const [firstname, setFirstname] = useState("");
   const [birthday, setBirthday] = useState("");
@@ -19,25 +22,92 @@ export default function NewPatient() {
   const [loading, setLoading] = useState(false);
   const token = useLogin().token;
   const updateContext = useUpdatePatients().update;
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [snackbarMsg, setSnackbarMsg] = useState("");
+
+  function checkValues(values) {
+    if (values.sex === null || values.sex === undefined || values.sex === "") {
+      return {
+        check: false,
+        message: "Vous devez indiquer le genre de la personne.",
+      };
+    }
+
+    if (values.lastname === "")
+      return {
+        check: false,
+        message: "Vous devez entrer au moins un nom de famille.",
+      };
+
+    // we check if there isn't already a patient with same first and last name, and whose birthday is less an a year to this one
+    var index = patients.findIndex(
+      (element) =>
+        capitalize(element?.lastname) === capitalize(values?.lastname) &&
+        capitalize(element?.firstname) === capitalize(values?.firstname)
+    );
+    if (index !== -1) {
+      // now we need to look at the birthday. Either the new patient AND the old one don't have birthday sets, in which case there's gonna be an issue
+      if (values.birthday === "" || values.birthday === null) {
+        return {
+          check: false,
+          message:
+            "Il existe déjà un patient avec le même nom et prénom. Merci d'indiquer un deuxième prénom ou bien une date de naissance afin de les distinguer.",
+        };
+      }
+      if (
+        values.birthday !== "" &&
+        values.birthday !== null &&
+        patients[index]?.birthday !== ""
+      ) {
+        // then we need to make sure that there is at least one year between their two birthdate to make sure at no point they will have the same age.
+        var difference =
+          new Date(values.birthday).getTime() -
+          new Date(patients[index]?.birthday).getTime();
+        var days = Math.abs(Math.ceil(difference / (1000 * 3600 * 24)));
+        if (days < 365) {
+          return {
+            check: false,
+            message:
+              "Il existe déjà un patient avec le même nom, prénom, et moins d'un an de différence d'âge. Merci d'indiquer un deuxième prénom afin de les distinguer.",
+          };
+        }
+      }
+      // Finally if one has a birthdate selected and not the other one doesn't it's fine because we can distinguish them based on that,
+      // so we carry on our checks
+    }
+
+    return { check: true };
+  }
 
   async function submitForm() {
     // add checks
+    const check = checkValues({
+      firstname: firstname,
+      lastname: name,
+      birthday: birthday,
+      sex: sex,
+    });
+    if (check.check) {
+      setLoading(true);
+      const result = await addNewPatient(firstname, name, birthday, sex);
+      if (result) {
+        // on confirme dans la snackbar
 
-    setLoading(true);
-    const result = await addNewPatient(firstname, name, birthday, sex);
-    if (result) {
-      // on confirme dans la snackbar
-
-      // on met à jour le state
-      setName("");
-      setFirstname("");
-      setBirthday("");
-      setSex();
-
-      // on met à jour le contexte
-      await updateContext(token);
+        // on met à jour le contexte
+        await updateContext(token);
+        // on met à jour le state
+        setName("");
+        setFirstname("");
+        setBirthday("");
+        setSex();
+        setSnackbarMsg("Patient ajouté.");
+        setShowSnackbar(true);
+      } else {
+        // on met un message d'erreur dans la snackbar
+      }
     } else {
-      // on met un message d'erreur dans la snackbar
+      setSnackbarMsg(check.message);
+      setShowSnackbar(true);
     }
     setLoading(false);
   }
@@ -80,75 +150,85 @@ export default function NewPatient() {
   }
 
   return (
-    <View style={styles.item}>
-      <Title style={styles.text}>Nouveau Patient</Title>
-      <View>
-        <TextInput
-          style={styles.textInput}
-          activeUnderlineColor="#1098AD"
-          label="Nom"
-          value={name}
-          onChangeText={(text) => setName(text)}
-          type="outlined"
-        />
-        <TextInput
-          style={styles.textInput}
-          activeUnderlineColor="#1098AD"
-          label="Prénom"
-          value={firstname}
-          onChangeText={(text) => setFirstname(text)}
-          type="outlined"
-        />
-        <TextInput
-          style={styles.textInput}
-          activeUnderlineColor="#1098AD"
-          label="Date de naissance"
-          placeholder="JJ/MM/AAAA"
-          value={birthday}
-          onChangeText={(text) => handleBirthday(text)}
-          type="outlined"
-        />
-        <RadioButton.Group
-          onValueChange={(newValue) => setSex(newValue)}
-          value={sex}
-        >
-          <View style={styles.radioButtonGroup}>
-            <View style={styles.radioButton}>
-              <Text style={styles.text}>Homme</Text>
-              <RadioButton
-                value="homme"
-                uncheckedColor="#ffffff"
-                color="#1098AD"
-              />
+    <>
+      <View style={styles.item}>
+        <Title style={styles.text}>Nouveau Patient</Title>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            activeUnderlineColor="#1098AD"
+            label="Nom"
+            value={name}
+            onChangeText={(text) => setName(text)}
+            type="outlined"
+          />
+          <TextInput
+            style={styles.textInput}
+            activeUnderlineColor="#1098AD"
+            label="Prénom"
+            value={firstname}
+            onChangeText={(text) => setFirstname(text)}
+            type="outlined"
+          />
+          <TextInput
+            style={styles.textInput}
+            activeUnderlineColor="#1098AD"
+            label="Date de naissance"
+            placeholder="JJ/MM/AAAA"
+            value={birthday}
+            onChangeText={(text) => handleBirthday(text)}
+            type="outlined"
+          />
+          <RadioButton.Group
+            onValueChange={(newValue) => setSex(newValue)}
+            value={sex}
+          >
+            <View style={styles.radioButtonGroup}>
+              <View style={styles.radioButton}>
+                <Text style={styles.text}>Homme</Text>
+                <RadioButton
+                  value="homme"
+                  uncheckedColor="#ffffff"
+                  color="#1098AD"
+                />
+              </View>
+              <View style={styles.radioButton}>
+                <Text style={styles.text}>Femme</Text>
+                <RadioButton
+                  value="femme"
+                  uncheckedColor="#ffffff"
+                  color="#1098AD"
+                />
+              </View>
+              <View style={styles.radioButton}>
+                <Text style={styles.text}>Autre</Text>
+                <RadioButton
+                  value="autre"
+                  uncheckedColor="#ffffff"
+                  color="#1098AD"
+                />
+              </View>
             </View>
-            <View style={styles.radioButton}>
-              <Text style={styles.text}>Femme</Text>
-              <RadioButton
-                value="femme"
-                uncheckedColor="#ffffff"
-                color="#1098AD"
-              />
-            </View>
-            <View style={styles.radioButton}>
-              <Text style={styles.text}>Autre</Text>
-              <RadioButton
-                value="autre"
-                uncheckedColor="#ffffff"
-                color="#1098AD"
-              />
-            </View>
-          </View>
-        </RadioButton.Group>
-        <Button
-          loading={loading}
-          color="#1098AD"
-          mode="contained"
-          onPress={() => submitForm()}
-        >
-          AJOUTER
-        </Button>
+          </RadioButton.Group>
+          <Button
+            loading={loading}
+            color="#1098AD"
+            mode="contained"
+            onPress={() => submitForm()}
+          >
+            AJOUTER
+          </Button>
+        </View>
       </View>
-    </View>
+      <Snackbar
+        visible={showSnackbar}
+        onDismiss={() => setShowSnackbar(false)}
+        duration={5000}
+        style={{ backgroundColor: "#E3FAFC" }}
+      >
+        <Text style={{ color: "black" }}>{snackbarMsg}</Text>
+      </Snackbar>
+    </>
   );
 }
 
